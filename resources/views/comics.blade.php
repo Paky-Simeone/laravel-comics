59b351aba846a34dc5e1fca1398b0e5158bf0fe4
Paky-Simeone <pasquale.simeone.46@gmail.com>
2024-03-19 14:40:34 +0100

    final commit

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -3,10 +3,10 @@
 @section('main-content')
 <div class="colore_back">
   <h1 class="titolo-fumetti">Comics</h1>
-  <div class="row g-4">
+  <div class="row g-2">
       @foreach($comics as $comic)
       <div class="col-2">
-          <div class="card">
+          <div class="card m-3">
             <img src="{{ $comic['thumb'] }}" class="card-img-top" alt="{{$comic['series']}}">
             <div>
               <p>{{ $comic['series'] }}</p>
@@ -19,6 +19,42 @@
   <div class="button-section">
     <button class="btn btn-primary">LOAD MORE</button>
   </div>
+</div>
+<div class="body-bottom">
+    <div class="container">
+        <ul>
+            <li>
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-comics-digital-comics.png')}}" alt="buy comics">
+                    <span>DIGITAL COMICS</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-comics-merchandise.png')}}" alt="buy comics">
+                    <span>DC MERCHANDISE<span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-comics-subscriptions.png')}}" alt="buy comics">
+                    <span>SUBSCRIPTION</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-comics-shop-locator.png')}}" alt="buy comics">
+                    <span>COMIC SHOP LOCATOR</span>
+                </a>
+            </li>
+            <li>
+                <a href="#">
+                    <img src="{{Vite::asset('resources/images/buy-dc-power-visa.svg')}}" alt="buy comics">
+                    <span>DC POWER VISA</span>
+                </a>
+            </li>
+        </ul>
+    </div>
 </div>
 
 @endsection
